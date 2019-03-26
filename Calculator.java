65b96bc0f3c15f20aa9abaa84da132011dbecb2b
DarthChurros <tnarayan00@stuy.edu>
2019-03-26 07:34:38 -0400

    calculate works I think

--- Calculator.java
+++ Calculator.java
@@ -2,38 +2,42 @@ public class Calculator {
   public static double eval(String s) {
     MyDeque<Double> stack = new MyDeque<Double>();
     String arg = "";
-    double temp;
     for (int i = 0; i < s.length(); i++) {
       if (s.charAt(i) != ' ') {
         arg += s.charAt(i);
       } else {
-        System.out.println(stack);
-        switch (arg) {
-          case "+":
-            stack.addLast(stack.removeLast() + stack.removeLast());
-            break;
-          case "-":
-            temp = stack.removeLast();
-            stack.addLast(stack.removeLast() - temp);
-            break;
-          case "*":
-            stack.addLast(stack.removeLast() * stack.removeLast());
-            break;
-          case "/":
-            temp = stack.removeLast();
-            stack.addLast(stack.removeLast() / temp);
-            break;
-          case "%":
-            temp = stack.removeLast();
-            stack.addLast(stack.removeLast() % temp);
-            break;
-          default:
-            stack.addLast(Double.parseDouble(arg));
-        }
+        operate(stack, arg);
         arg = "";
       }
     }
+    operate(stack, arg);
     return stack.getLast();
+  }
+
+  private static void operate(MyDeque<Double> stack, String arg) {
+    double temp;
+    switch (arg) {
+      case "+":
+        stack.addLast(stack.removeLast() + stack.removeLast());
+        break;
+      case "-":
+        temp = stack.removeLast();
+        stack.addLast(stack.removeLast() - temp);
+        break;
+      case "*":
+        stack.addLast(stack.removeLast() * stack.removeLast());
+        break;
+      case "/":
+        temp = stack.removeLast();
+        stack.addLast(stack.removeLast() / temp);
+        break;
+      case "%":
+        temp = stack.removeLast();
+        stack.addLast(stack.removeLast() % temp);
+        break;
+      default:
+        stack.addLast(Double.parseDouble(arg));
+    }
   }
 
   public static void main(String[] args) {
